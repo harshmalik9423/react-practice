c1dacda2ca7fe4a1bc3ca7191a4663bd3e2d248a
Harsh Malik <harshmalik2013@gmail.com>
2020-06-26 00:46:40 +0530

    Added functionality to HomePageComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from "./DishdetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -15,7 +19,9 @@ class Main extends Component {
 
         this.state = {
         dishes : DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS
         };
     }
 
@@ -32,7 +38,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader= {this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
     return (
@@ -41,6 +50,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
