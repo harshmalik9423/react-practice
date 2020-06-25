f9c764be08dd4e3a144c28e7f194d29410362224
Harsh Malik <harshmalik2013@gmail.com>
2020-06-26 01:40:16 +0530

    Using URL parameters to load dish details

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,15 +25,6 @@ class Main extends Component {
         };
     }
 
-    renderDish(dishId) {
-        if (dishId != null) {
-          return <DishDetail 
-            dish={this.state.dishes.filter((dish) => (dish.id === this.state.selectedDish))[0]}/>;
-        } else {
-          return <div></div>;
-        }
-      }
-
   render() {
 
     const HomePage = () => {
@@ -44,12 +35,22 @@ class Main extends Component {
         />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        />
+      );
+    }
+
     return (
       <div>
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
