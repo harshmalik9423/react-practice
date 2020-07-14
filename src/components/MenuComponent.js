d4cfd111b7944283a4d1bbbed964d14c0df0430e
Harsh Malik <harshmalik2013@gmail.com>
2020-07-15 01:30:39 +0530

    Added a json-server and fetching data from it

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from "../shared/sharedUrl";
 
 function RenderMenuItem({ dish, onClick }) {
     return(
         <Card>
             <Link to={`/menu/${dish.id}`} >
-                <CardImg src={dish.image} alt={dish.name} />
+                <CardImg src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay className="ml-5">
                     <CardTitle><h3> {dish.name} </h3></CardTitle>
                 </CardImgOverlay>
